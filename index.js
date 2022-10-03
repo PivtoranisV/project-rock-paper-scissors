1d4f7e4f311178b062699bdc69db2ff55881e82f
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-10-04 00:15:33 +0400

    Added function for one round

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ function getUserChoice() {
   );
   return userSelection.toLowerCase();
 }
+
+function playRound(playerSelection, computerSelection) {
+  //   console.log(computerSelection);
+  //   console.log(playerSelection);
+  if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose, scissors beats paper";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win, paper beats rock";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win, scissors beats paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose, rock beats scissors";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose, paper beats rock";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win, rock beats scissors";
+  } else {
+    return `Tie, you both choose ${playerSelection}`;
+  }
+}
+
+const playerSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
