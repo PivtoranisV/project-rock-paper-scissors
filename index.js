b94112a21fb48983ab9b9a2a9426a2158d66efdc
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-10-03 23:42:10 +0400

    Added function for user choice

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const option = ["Paper", "Scissors", "Rock"];
+const option = ["paper", "scissors", "rock"];
 
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3);
   return option[randomIndex];
+}
+
+function getUserChoice() {
+  const userSelection = prompt(
+    "What is your choice? Paper or Scissors or maybe Rock? "
+  );
+  return userSelection.toLowerCase();
 }
