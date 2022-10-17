0845f2e7f24d102b181867fc40665083ea18a85d
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-10-17 23:37:41 +0400

    Added ged Winner function

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const displayComputerChoice = document.querySelector('.computer__choice');
 const displayRoundResult = document.querySelector('.round-result');
 const displayPlayerScore = document.querySelector('.player__score');
 const displayComputerScore = document.querySelector('.computer__score');
+const displayGameResult = document.querySelector('.result-header');
+const resultWindow = document.querySelector('.result');
+const playAgain = document.querySelector('.play-again');
 let playerSelection;
 let computerSelection;
 let playerScore = 0;
@@ -16,6 +19,18 @@ let computerScore = 0;
 startGameBtn.addEventListener('click', () => {
   startGameWindow.classList.add('hide');
   gameWindow.classList.remove('hide');
+});
+
+playAgain.addEventListener('click', () => {
+  resultWindow.classList.add('hide');
+  gameWindow.classList.remove('hide');
+  displayPlayerChoice.textContent = '';
+  displayComputerChoice.textContent = '';
+  displayRoundResult.textContent = '';
+  displayPlayerScore.textContent = '';
+  displayComputerScore.textContent = '';
+  playerScore = 0;
+  computerScore = 0;
 });
 
 weaponButtons.forEach((weaponButton) =>
@@ -31,6 +46,19 @@ function getComputerChoice() {
   return computerSelection;
 }
 
+//function to display winner
+function getWinner(playerScore, computerScore) {
+  if (playerScore === 5) {
+    displayGameResult.textContent = 'Player is a Winner';
+    resultWindow.classList.remove('hide');
+    gameWindow.classList.add('hide');
+  } else if (computerScore === 5) {
+    displayGameResult.textContent = 'Computer is a Winner';
+    resultWindow.classList.remove('hide');
+    gameWindow.classList.add('hide');
+  }
+}
+
 // function for one round
 function playRound(playerSelection) {
   getComputerChoice();
@@ -41,58 +69,28 @@ function playRound(playerSelection) {
     displayRoundResult.textContent = 'You lose, scissors beats paper';
     computerScore++;
     displayComputerScore.textContent = computerScore;
-    winner = 'Computer';
   } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
     displayRoundResult.textContent = 'You win, paper beats rock';
     playerScore++;
     displayPlayerScore.textContent = playerScore;
-    winner = 'Player';
   } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
     displayRoundResult.textContent = 'You win, scissors beats paper';
     playerScore++;
     displayPlayerScore.textContent = playerScore;
-    winner = 'Player';
   } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
     displayRoundResult.textContent = 'You lose, rock beats scissors';
     computerScore++;
     displayComputerScore.textContent = computerScore;
-    winner = 'Computer';
   } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
     displayRoundResult.textContent = 'You lose, paper beats rock';
     computerScore++;
     displayComputerScore.textContent = computerScore;
-    winner = 'Computer';
   } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
     displayRoundResult.textContent = 'You win, rock beats scissors';
     playerScore++;
     displayPlayerScore.textContent = playerScore;
-    winner = 'Player';
   } else {
     displayRoundResult.textContent = `Tie, you both choose ${playerSelection}`;
   }
-  return winner;
+  getWinner(playerScore, computerScore);
 }
-
-//function for the game with score track and winner declaration
-// function game(times) {
-//   let playerScore = 0;
-//   let computerScore = 0;
-//   for (let i = 0; i < times; i++) {
-//     const playerSelection = getUserChoice();
-//     const computerSelection = getComputerChoice();
-//     playRound(playerSelection, computerSelection);
-//     if (winner === 'Player') {
-//       playerScore++;
-//     } else if (winner === 'Computer') {
-//       computerScore++;
-//     }
-//   }
-//   console.log(
-//     `Your score is ${playerScore} and computer score is ${computerScore}`
-//   );
-//   return playerScore > computerScore
-//     ? 'Player is a Winner'
-//     : 'Computer is a Winner';
-// }
-
-//console.log(game(3));
